f28fc0f8114d4def6aa5c4f255178b74816d661a
Unknown <davidestiven17@gmail.com>
2018-03-06 12:43:48 -0500

    primer ejercicio

--- clase_6/main.cpp
+++ clase_6/main.cpp
@@ -1,9 +1,34 @@
-#include <iostream>
-
-using namespace std;
+#include <iostream> //biblioteca I/O
+#include <math.h> //biblioteca matematica
+using namespace std; //palabras reservadas
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int contador=0,suma=0;
+
+    while (contador<50)
+    {
+        suma=suma+contador;
+        contador=contador+1;
+        cout<<" - "<<contador<<endl;
+    }
+
+    cout<<"suma: "<<suma<<endl;
+    suma=pow(suma,3);
+    cout<<"suma al cubo: "<<suma<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
